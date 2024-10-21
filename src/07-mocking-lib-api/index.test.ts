8ae82797045a5c0c778c6aafb702be3b123d4c61
Maxim Tsymanovich <m.tsymanovich@syberry.com>
2024-10-22 01:32:08 +0300

    resolve tests

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,17 +1,45 @@
-// Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+import axios from 'axios';
+import { throttledGetDataFromApi } from './index';
 
 describe('throttledGetDataFromApi', () => {
+  const mockData = { data: 'data' };
+  const path = '/path';
+
+  beforeEach(() => {
+    jest.spyOn(axios.Axios.prototype, 'get').mockResolvedValue(mockData);
+  });
+
+  beforeAll(() => {
+    jest.useFakeTimers();
+  });
+
+  afterAll(() => {
+    jest.useRealTimers();
+  });
+
   test('should create instance with provided base url', async () => {
-    // Write your test here
+    const createSpy = jest.spyOn(axios, 'create');
+
+    await throttledGetDataFromApi(path);
+    jest.runAllTimers();
+    expect(createSpy).toHaveBeenCalledWith({
+      baseURL: expect.any(String),
+    });
   });
 
   test('should perform request to correct provided url', async () => {
-    // Write your test here
+    const getSpy = jest
+      .spyOn(axios.Axios.prototype, 'get')
+      .mockResolvedValueOnce(mockData);
+
+    await throttledGetDataFromApi(path);
+    jest.runAllTimers();
+    expect(getSpy).toHaveBeenCalledWith(path);
   });
 
   test('should return response data', async () => {
-    // Write your test here
+    const result = await throttledGetDataFromApi(path);
+    jest.runAllTimers();
+    expect(result).toEqual(mockData.data);
   });
 });
--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,19 @@
 // Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
 
 describe('generateLinkedList', () => {
-  // Check match by expect(...).toStrictEqual(...)
+  const elements = [2, 4, 6];
+
+  const result = {
+    value: 2,
+    next: { value: 4, next: { value: 6, next: { value: null, next: null } } },
+  };
+
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    expect(generateLinkedList(elements)).toStrictEqual(result);
   });
 
-  // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    expect(generateLinkedList(elements)).toMatchSnapshot();
   });
 });
